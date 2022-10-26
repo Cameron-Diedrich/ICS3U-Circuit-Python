b503cb82256a8b9b1c8c69396579de8ffb898ddd
Cameron-Diedrich <113041454+Cameron-Diedrich@users.noreply.github.com>
2022-10-26 10:51:33 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Created by: Cameron Diedrich
+# Created on: Oct 2022
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # his function is the main game game_scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.bank.from_bmp16("space_aiens_background.bmp")
+
+    # set the background to image 0 in the  image bank
+    #   and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    #create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = scene.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all sprites
+    #   most likely you will only render the background once per scene
+    game.render_block()
+
+    # respect forever, game loop
+    while True:
+        pass # just a place holder for now
+
+if __name__ == "__main__":
+    game_scene()
